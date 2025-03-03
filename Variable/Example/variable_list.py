0108c6ad43f8de12d06d4873ced8fbc4db728b68
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 18:00:25 +0530

    Create variable_list.py

---
+++ Variable/Example/variable_list.py
@@ -0,0 +1,21 @@
+# Creating a List
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing Elements
+print("First Fruit:", fruits[0])
+print("Last Fruit:", fruits[-1])
+
+# Modifying Elements
+fruits[1] = "blueberry"
+print("Modified List:", fruits)
+
+# Adding Elements
+fruits.append("orange")
+print("After Append:", fruits)
+
+# Removing Elements
+fruits.remove("cherry")
+print("After Remove:", fruits)
+
+# List Length
+print("Number of Fruits:", len(fruits))
