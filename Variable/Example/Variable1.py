6667d11f2e0da62b24485c6d5b44d43b0fa37e45
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 17:55:14 +0530

    Create Variable1.py

---
+++ Variable/Example/Variable1.py
@@ -0,0 +1,19 @@
+# Integer to Float
+num_int = 10
+num_float = float(num_int)
+print("Integer to Float:", num_float)
+
+# Float to Integer
+num_float = 10.7
+num_int = int(num_float)
+print("Float to Integer:", num_int)
+
+# Integer to String
+num_int = 42
+num_str = str(num_int)
+print("Integer to String:", num_str)
+
+# String to Integer
+num_str = "123"
+num_int = int(num_str)
+print("String to Integer:", num_int)
