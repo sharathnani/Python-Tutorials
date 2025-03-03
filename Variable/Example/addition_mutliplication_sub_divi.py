62248be3dbf4a2483132daa91fd7194bb13838f3
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 20:05:22 +0530

    Create addition_mutliplication_sub_divi.py

---
+++ Variable/Example/addition_mutliplication_sub_divi.py
@@ -0,0 +1,16 @@
+x=float(input(""))
+y=float(input("\n enter y"))
+z=float(input("\n enter z"))
+print("\n",x+y)
+print(y-z)
+print(x*y)
+print(x/z)
+print(x%z)
+x==z
+print(x)
+
+
+print(x>z)
+print(x==z)
+print(x<4 or y>6)
+print(x>5 and y<6)
