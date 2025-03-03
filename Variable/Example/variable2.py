a14dae1219e898159dcc38a76a3becc797671263
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 17:57:20 +0530

    Create variable2.py

---
+++ Variable/Example/variable2.py
@@ -0,0 +1,13 @@
+# Concatenation
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+# String Methods
+sentence = "Python Programming is Fun!"
+print("Upper Case:", sentence.upper())
+print("Lower Case:", sentence.lower())
+print("Title Case:", sentence.title())
+print("Length of String:", len(sentence))
+print("Replace 'Fun' with 'Awesome':", sentence.replace("Fun", "Awesome"))
