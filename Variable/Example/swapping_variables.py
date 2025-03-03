b4b6bc6a40ecd1821dd577d1ec20a079818ba26e
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 19:25:17 +0530

    Create swapping_variables.py

---
+++ Variable/Example/swapping_variables.py
@@ -0,0 +1,11 @@
+# Multiple Assignments
+x, y, z = 10, 20, 30
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
+# Swapping Variables
+x, y = y, x
+print("After Swapping:")
+print("x:", x)
+print("y:", y)
