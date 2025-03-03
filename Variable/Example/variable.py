e77dbe8f3d847c3bb7afde484e63b703624f1ccf
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 17:53:34 +0530

    Create variable.py

---
+++ Variable/Example/variable.py
@@ -0,0 +1,17 @@
+# Integer
+age = 25
+
+# Float
+height = 5.9
+
+# String
+name = "Alice"
+
+# Boolean
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
