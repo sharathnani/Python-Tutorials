ab182f4f2cac9506b210f77817454f6eeb974871
sharathnani <91570664+sharathnani@users.noreply.github.com>
2025-03-03 18:08:54 +0530

    Create variable3.py

---
+++ Variable/Example/variable3.py
@@ -0,0 +1,12 @@
+# Creating a Tuple
+coordinates = (10.0, 20.0)
+
+# Accessing Elements
+print("X Coordinate:", coordinates[0])
+print("Y Coordinate:", coordinates[1])
+
+# Tuples are immutable, so you can't modify them
+# coordinates[0] = 15.0  # This will raise an error
+
+# Tuple Length
+print("Number of Coordinates:", len(coordinates))
